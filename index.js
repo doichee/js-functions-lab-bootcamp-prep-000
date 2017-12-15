6586e76eb321da728bb092dcc80946b58f229f57
Ben Deutsch <ben.deutschiii@gmail.com>
2017-12-15 00:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  happyHolidaysTo("you")
   return 'Happy holidays, ${name}!'
+  happyHolidaysTo("you")
 }
